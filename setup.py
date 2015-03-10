ad3c449be1e9306c990fb05e397590f7a14556e0
Dan McClory <danmcclory@gmail.com>
2015-03-10 13:42:42 -0400

    start with setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+requirements = [
+
+
+]
+
+setup(name='schematics-extensions',
+      version='0.0.1',
+      description='Extensions for the Schematics library',
+      author='Picwell',
+      author_email='dev@picwell.com',
+      url='http://github.com/picwell/schematics-extensions',
+      install_requires=requirements)
