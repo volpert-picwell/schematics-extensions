a7ba15c7026c94dc8074fb2403fe997e217eeadc
Dan McClory <danmcclory@gmail.com>
2015-03-10 17:18:31 -0400

    PR feedback

--- schematics_extensions/__init__.py
+++ schematics_extensions/__init__.py
@@ -1,6 +1,3 @@
-from schematics.types import (
-    IntType, StringType, FloatType, BooleanType, LongType)
-
 from .mockable_list_type import MockableListType
 from .immutable_model import ImmutableModel
 from .mockable_model_type import MockableModelType
