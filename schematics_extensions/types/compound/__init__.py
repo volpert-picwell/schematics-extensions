9f71a4f6b7f37f642cb9124675a2a22ea33dff22
Dustin Ingram <di@users.noreply.github.com>
2015-06-04 11:21:55 -0400

    Fix the ListType validate_items function

--- schematics_extensions/types/compound/__init__.py
+++ schematics_extensions/types/compound/__init__.py
@@ -1,4 +1,5 @@
 from schematics.types.compound import DictType, ListType, ModelType
+from schematics.exceptions import ValidationError
 
 
 class DictType(DictType):
@@ -13,6 +14,26 @@ class ListType(ListType):
     @classmethod
     def null(self):
         return []
+
+    # TODO: Remove the following hacks once Schematics 1.0.5 is released.
+
+    # Remove the broken `validate_items` function
+    ListType._validators = [
+        v for v in ListType._validators
+        if v.__name__ is not ListType.validate_items.__name__
+    ]
+
+    # Create a new `validate_items` function for the ListType
+    def validate_items(self, items):
+        errors = []
+        for item in items:
+            try:
+                self.field.validate(item)
+            except ValidationError as exc:
+                errors.append(exc.messages)
+
+        if errors:
+            raise ValidationError(errors)
 
 
 class ModelType(ModelType):
